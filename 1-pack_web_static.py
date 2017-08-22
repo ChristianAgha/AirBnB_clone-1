6217064190a6cd7f9febba9f456f2998669a7e6d
Christian Agha <christianagha@gmail.com>
2017-08-22 00:08:29 -0700

    adding prob #1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive
+from the contents of the web_static folder
+"""
+from fabric.api import *
+from time import strftime
+
+
+def do_pack():
+    """Makes .tgz archive from web_static folder"""
+    now = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static".format(now))
+        return("versions/web_static_{}.tgz web_static".format(now))
+    except:
+        return(None)
