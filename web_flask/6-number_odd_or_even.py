da9bffc7a5b9a876eeaf6c8905b8a77ee2ae7ad3
Christian Agha <christianagha@gmail.com>
2017-09-06 18:07:35 -0700

    adding problem #6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+from flask import Flask, abort, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """
+    prints Hello HBNB!
+    """
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_HBNB():
+    """
+    prints HBNB!
+    """
+    return("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_C(text):
+    """
+    Displays "C " followed by value of text
+    """
+    text = "C " + text.replace('_', ' ')
+    return(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_py(text='is cool'):
+    """
+    Displays "Python " followed by value of text
+    """
+    py_text = "Python " + text.replace('_', ' ')
+    return(py_text)
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def display_n(n):
+    """
+    Displays n is a number" only if n is an integer
+    """
+    try:
+        return("{} is a number".format(int(n)))
+    except:
+        abort(404)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_template(n):
+    """
+    display an HTML page only if n is an int
+    """
+    try:
+        return render_template('5-number.html', n=n)
+    except:
+        abort(404)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def display_odd_even(n):
+    """
+    display an HTML page only if n is an int
+    """
+    try:
+        return render_template('6-number_odd_or_even.html', n=n)
+    except:
+        abort(404)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
